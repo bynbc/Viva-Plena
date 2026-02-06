b556c310ef8f732bcd570488b7c3879b2db29ed8
bynbc <nicolasbritoconcept@gmail.com>
2026-02-06 14:54:53 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,17 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// O SEGREDO ESTÁ AQUI EMBAIXO:
+import './index.css' 
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
