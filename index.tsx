ab1f097572b3be74e6f1d07ed1c6ac49e11d9155
bynbc <nicolasbritoconcept@gmail.com>
2026-02-09 22:54:15 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-// O SEGREDO ESTÁ AQUI EMBAIXO:
-import './index.css' 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
+// Importa os provedores (o cérebro e a segurança)
+import { BrainProvider } from './context/BrainContext';
+import { AuthProvider } from './context/AuthContext';
+
+// AQUI ESTAVA O ERRO: O render precisa ter o conteúdo dentro!
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrainProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrainProvider>
+  </React.StrictMode>
+);
