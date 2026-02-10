75512d6120a90fd3bd97e568be14442bb467240a
bynbc <nicolasbritoconcept@gmail.com>
2026-02-09 22:43:19 -0300

    Update QuickActionModals.tsx

--- components/QuickActionModals.tsx
+++ components/QuickActionModals.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { useBrain } from '../context/BrainContext';
-
-// Importação dos Modais
 import NewPatientModal from './NewPatientModal';
 import NewRecordModal from './NewRecordModal';
 import NewOccurrenceModal from './NewOccurrenceModal';
 import NewAgendaModal from './NewAgendaModal';
 import NewDocumentModal from './NewDocumentModal';
-import NewMedicationModal from './NewMedicationModal'; // <--- O NOVO INTEGRANTE
+import NewMedicationModal from './NewMedicationModal';
+// Se tiver outros modais (como Inventory), importe aqui também
 
 const QuickActionModals: React.FC = () => {
-  const { brain } = useBrain();
+  const { brain, setQuickAction } = useBrain();
   const { quickAction } = brain.ui;
 
   if (!quickAction) return null;
 
-  // Renderiza o modal correto baseado na ação clicada
   switch (quickAction) {
     case 'new_patient':
       return <NewPatientModal />;
+      
     case 'new_record':
-      return <NewRecordModal />;
+      // CORREÇÃO: Passando a prop obrigatória onClose
+      return <NewRecordModal onClose={() => setQuickAction(null)} />;
+      
     case 'new_occurrence':
-      return <NewOccurrenceModal />;
+      // CORREÇÃO: Passando a prop obrigatória onClose
+      return <NewOccurrenceModal onClose={() => setQuickAction(null)} />;
+      
     case 'new_agenda':
       return <NewAgendaModal />;
+      
     case 'new_document':
       return <NewDocumentModal />;
-    case 'new_medication': // <--- AQUI ESTÁ A LIGAÇÃO
+      
+    case 'new_medication':
       return <NewMedicationModal />;
+      
     default:
       return null;
   }
