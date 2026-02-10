c8abadf4b1f7abc180f13f175e4f200495478ff6
bynbc <nicolasbritoconcept@gmail.com>
2026-02-09 23:01:48 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Importa os provedores (Cérebro e Login)
+// Importa os provedores
 import { BrainProvider } from './context/BrainContext';
 import { AuthProvider } from './context/AuthContext';
 
-// AQUI ESTAVA O ERRO: Agora preenchemos o render com o App e os Provedores
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrainProvider>
